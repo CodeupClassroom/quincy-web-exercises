a50ed1e79f8e8626510e7d7c3ced94ae63e0b922
Justin Reich <justinreich.dev@gmail.com>
2017-06-15 11:27:35 -0500

    Updated solutions for conditional exercises.

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -14,19 +14,21 @@ var favorite = 'blue'; // TODO: change this to your favorite color from the list
 //       Example: Blue is the color of the sky.
 
 console.log("The random color chosen is " + color);
-if(color === "red") {
-    console.log("Roses are red");
+
+if (color === "red") {
+	console.log("Roses are red");
 } else if (color === "orange") {
-    console.log("Orange you glad I didn't say banana.");
-} else if(color === "yellow") {
-    console.log("Watch out where the Huskies go and don't you eat that yellow snow.");
-} else if(color === "green") {
-    console.log("Green is the color of kale. Yay. Tasty kale!");
-} else if(color === "blue") {
-    console.log("Blue roses are blue");
+	console.log("Orange you glad I didn't say banana.");
+} else if (color === "yellow") {
+	console.log("Watch out where the Huskies go and don't you eat that yellow snow.");
+} else if (color === "green") {
+	console.log("Green is the color of kale.");
+} else if (color === "blue") {
+	console.log("Blue roses are blue.");
 } else {
-    console.log("I don't know anything about that color.")
+	console.log("I don't know anything about that color.");
 }
+
 
 // TODO: Have a final else that will catch indigo and violet.
 // TODO: In the else, log: I do not know anything by that color.
@@ -34,8 +36,15 @@ if(color === "red") {
 // TODO: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
 
-console.log("The random color generated is " + color);
-console.log("My favorite color is " + favorite);
+console.log("My favorite color is: " + favorite);
 
-console.log(color == favorite ? "The random color matches!" : "Random and favorite colors are different.");
+var outcome = (color === favorite) ? "The random color matches!" : "Random and favorite colors are different";
+
+console.log(outcome);
+
+
+
+
+
+
 
--- public/js/switch.js
+++ public/js/switch.js
@@ -12,68 +12,94 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 // TODO: create a default case that will catch indigo and violet
 // TODO: for the default case, log: I do not know anything by that color.
 
-switch (color) {
-    case 'red':
-        console.log("Roses are red, yo!");
-        break;
-    case 'orange':
-        console.log("Orange you glad I didn't say banana.");
-        break;
-    case 'yellow':
-        console.log("Watch out where the huskies go and don't you eat that yellow snow!!!");
-        break;
-    case 'green':
-        console.log("Kale is green. Yay, kale!");
-        break;
-    case 'blue':
-        console.log("Blue roses are blue.");
-        break;
-    default:
-        console.log("I don't know anything about that color.");
-        break;
-}
+// switch(color) {
+//     case "Red":
+//         console.log("Roses are red");
+//         break;
+//     case "orange":
+//         console.log("Orange you glad I didn't say banana.")
+//         break;
+//     case "yellow":
+//         console.log("Watch out where the Huskies go and don't you eat that yellow snow.");
+//         break;
+//     case "green":
+//         console.log("Green is the color of kale.");
+//         break;
+//     case "blue":
+//         console.log("Blue roses are blue.");
+//         break;
+//     default:
+//         console.log("I don't know anything about that color.");
+//         break;
+// }
 
-var cameron = 180;
-var ryan = 250;
-var george = 320;
-var discountedAmount;
-var finalTotal;
+ /*
 
-if(cameron > 200) {
-    discountedAmount = cameron * .1;
-    finalTotal = cameron - discountedAmount;
-    console.log("Cameron got a discount on " + cameron + " of " + discountedAmount);
-    console.log("Cameron's final payment is " + finalTotal);
-} else {
-    console.log("Cameron paid " + cameron + " and did not get a discount.");
-}   
 
-if(ryan > 200) {
-    discountedAmount = ryan * .1;
-    finalTotal = ryan - discountedAmount;
-    console.log("Ryan got a discount on " + ryan + " of " + discountedAmount);
-    console.log("Ryan's final payment is " + finalTotal);
-} else {
-    console.log("Cameron paid " + cameron + " and did not get a discount.");
-}   
+    HEB Discount
+    
 
-if(george > 200) {
-    discountedAmount = george * .1;
-    finalTotal = george - discountedAmount;
-    console.log("George got a discount on " + george + " of " + discountedAmount);
-    console.log("George's final payment is " + finalTotal);
-} else {
-    console.log("George paid " + george + " and did not get a discount.");
-}   
+ */
+
+
+// var cameron = 180;
+// var ryan = 250;
+// var george = 320;
+// var discountedAmount;
+// var finalTotal;
+
+// if (cameron > 200) {
+//     discountedAmount = cameron * .1;
+//     finalTotal = cameron - discountedAmount;
+//     console.log("Cameron got a discount on " + cameron + " of " + discountedAmount);
+// } else {
+//     console.log("Cameron paid " + cameron + " and did not have a discount");
+//     finalTotal = cameron;
+// }
+
+// console.log("Cameron had a final total of " + finalTotal);
+
+
+
+// if (ryan > 200) {
+//     discountedAmount = ryan * .1;
+//     finalTotal = ryan - discountedAmount;
+//     console.log("Ryan got a discount on " + ryan + " of " + discountedAmount);
+// } else {
+//     console.log("Ryan paid " + ryan + " and did not have a discount");
+//     finalTotal = ryan;
+// }
+
+// console.log("Cameron had a final total of " + finalTotal);
+
+
+// if (george > 200) {
+//     discountedAmount = george * .1;
+//     finalTotal = george - discountedAmount;
+//     console.log("George got a discount on " + george + " of " + discountedAmount);
+// } else {
+//     console.log("George paid " + george + " and did not have a discount");
+//     finalTotal = george;
+// }
+
+// console.log("Cameron had a final total of " + finalTotal);
 
 // flip a coin
-var flipACoin = Math.floor(Math.random()* 2)
 
-if(flipACoin == 0) {
-console.log("BUY THE CAR!");
-} else {
-console.log("Buy a house!");
-}
+// var flipACoin = Math.floor(Math.random()* 2);
+
+// console.log(flipACoin);
+
+// if (flipACoin === 1) {
+//     console.log("Buy a house!");
+// } else {
+//     console.log("Buy a car!");
+// }
+
+// var outcome = (flipACoin === 1) ? "Buy a house!" : "Buy a car!";
+
+// console.log(outcome);
+
 
 //Each customer is given a randomly generated "lucky number" between 0 and 6. 
 // If your lucky number is 0 you have no discount, 
@@ -83,63 +109,102 @@ console.log("Buy a house!");
 // if it's 5 you'll get all for free!. 
 // Log how much you will have to pay if your receipt is for $60. Every time you reload your page you should see a different result. Use a switch statement for this exercise.
 
-console.log("=====")
-console.log("Lucky number time!");
 
-var luckyNumber = Math.floor(Math.random()* 6)
-var subtotal = 60;
-var discount = 0;
-var newTotal;
 
-console.log("Your lucky number is " + luckyNumber);
+// var luckyNumber = Math.floor(Math.random() * 6);
+// var subtotal = 60;
+// var discount = 0;
+// var newTotal;
 
-switch(luckyNumber) {
-    case 0: 
-        console.log("You get no discount.");
-        discount = 0;
-        break;
-    case 1:
-        console.log("You get a 10 percent discount.");
-        discount = 0.1;
-        break;
-    case 2:
-        console.log("You get a 25 percent discount.");
-        discount = 0.25;
-        break;
-    case 4:
-        console.log("You get a fifty percent discount!");
-        discount = 0.5;
-        break;
-    case 5:
-        console.log("You get 100% discount!");
-        discount = 1.0;
-        break;
-    default:
-        console.log("This lucky number has no discount");
-        discount = 0;
-        break;
-}
 
-newTotal = subtotal - (subtotal * discount);
-console.log("Your final total after discount is " + newTotal);
+// console.log(luckyNumber);
 
-// using confirm 
-console.log("=============");
-var number = prompt("Please enter a number");
-if(isNaN(parseFloat(number))) {
-    console.log(number + " aint a number.");
+// switch(luckyNumber) {
+
+//     case 0:
+//         console.log("No discount");
+//         discount = 0;
+//         break;
+//     case 1:
+//         console.log("You get a 10 percent discount");
+//         discount = .1;
+//         break;
+//     case 2:
+//         console.log("You get a 25 percent discount");
+//         discount = .25;
+//         break;
+//     case 4:
+//         console.log("You get a 50 percent discount");
+//         discount = .5;
+//         break;
+//     case 5:
+//         console.log("You get 100%");
+//         discount = 1.0;
+//         break;
+//     default:
+//         console.log("No lucky number discount");
+// }
+
+
+// newTotal = subtotal - (subtotal * discount);
+// console.log("Your final total after discount is " + newTotal);
+
+
+
+// using confirm and prompt and alert
+/*
+    Write some JavaScript that uses a confirm dialog to ask the user if they would like to enter a number. 
+    If they click 'Ok', prompt the user for a number, then use 3 separate alerts to tell the user:
+
+    whether the number is even or odd
+    what the number plus 100 is
+    if the number is negative or positive
+    if what the user enters is not a number, use an alert to tell them that, and do not display any of the above information.
+*/
+
+
+var proceed = confirm("Would you like to enter a number?");
+
+if (proceed) {
+
+    var number = prompt("Please enter a number.");
+
+    if (isNaN(parseFloat(number))) {
+        console.log("Not a valid number.");   
+    } else {
+        if (number % 2 === 0) {
+            alert("Your number is even.");
+        } else {
+            alert("Your number is odd.")
+        }
+
+        alert("Your number plus 100 is " + (parseFloat(number) + 100));
+
+        if (number < 0) {
+            console.log(number + " is negative");
+        } else {
+            console.log(number + " is positive");
+        }
+    }
+
 } else {
-    if(number % 2 == 0) {
-        console.log(number + " is even");
-    } else {
-        console.log(number + " is odd")
-    }
-
-    console.log(number + " plus 100 is " + (number + 100));
-
-    if(number < 0) {
-        console.log(number + " is negative");
-    } else {
-        console.log(number + " is positive");
-    }
+    console.log("Another time then...");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
