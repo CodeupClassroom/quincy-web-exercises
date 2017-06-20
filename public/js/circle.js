4acd06d85393bfeac741fd251287d563594d2dd7
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-06-20 15:00:15 -0500

    Add circle "this" solution exercise walk-through

---
+++ public/js/circle.js
@@ -0,0 +1,45 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            return Math.PI * Math.pow(this.radius, 2); 
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            var area = this.getArea();
+            
+            if(doRounding) {
+                area = Math.round(area);
+            } 
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
+        }   
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
